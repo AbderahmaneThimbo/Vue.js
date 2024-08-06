586252c0ad138d785ccc7d7030c4c532784f5951
AbderahmaneThimbo <abderahmanethimbo@gmail.com>
2024-08-06 14:31:29 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template> 
+       <Hello :user="user"/>
+     
+       <!-- <div v-if="user.age > 15">
+         Mon age
+       </div>
+       <div v-else>
+         Else
+       </div> -->
+       <div :class="[user.age > 30 ? 'text-red' : 'text-green']">
+         {{ user.name }}
+       </div>
+       
+     <input type="text" v-model="user.name" v-on:input="display">
+     <input type="text" v-model="user.age">
+
+</template>
+
+<script setup>
+import Hello from './components/Hello.vue';
+
+import { reactive } from 'vue';
+
+const user = reactive({
+   name: 'Coders',
+   age:65,
+});
+
+const display = () => {
+   console.log(user.name,user.age);
+   
+}
+
+</script>
+
+
+
+<style scoped>
+
+.text-red {
+   color:red;
+}
+.text-green{
+   color: green;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+   <h1>Hello word ! </h1>
+   <p>{{ props.user.name }} {{ props.user.age }}</p>
+   
+ </template>
+ 
+ <script setup>
+
+const props = defineProps(['user']);
+ </script>
+ 
+ 
+ 
+ <style scoped>
+  h1{
+    color: blue;
+  }
+ </style>
+ 
